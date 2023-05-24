7f8402491b9d7667ba6d47fbe3b4ee350ec9ba76
Kevin Shaffer-Morrison <shafkevi@amazon.com>
2023-05-24 18:56:15 -0400

    adding some more basic pg API functionality for demonstration purposes

--- server.py
+++ server.py
@@ -21,6 +21,8 @@ try:
                             port=port,
                             password=password
                            )
+    conn.set_session(autocommit=True)
+
 except Exception as e:
     print("I am unable to connect to the database", e)
 
@@ -41,3 +43,42 @@ async def tables():
     tables = cursor.fetchall()
     cursor.close()
     return tables
+
+
+@app.get("/pg/init")
+async def pg_init():
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("create table if not exists items (key text primary key, value text)")
+    cursor.close()
+
+
+@app.get("/pg/destroy")
+async def pg_destroy():
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("drop table items")
+    cursor.close()
+
+
+
+@app.get("/pg/put/{key}/{value}")
+async def pg_put_key_value(key, value):
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("insert into items(key, value) VALUES('{key}','{value}') on conflict (key) do update set value = '{value}';".format(**{"key": key, "value": value}))
+    cursor.close()
+
+
+@app.get("/pg/get")
+async def pg_get():
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("select * from items;")
+    items = cursor.fetchall()
+    cursor.close()
+    return items
+
+@app.get("/pg/get/{key}")
+async def pg_get_key(key):
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("select * from items where key = '{key}';".format(**{"key": key}))
+    items = cursor.fetchall()
+    cursor.close()
+    return items
